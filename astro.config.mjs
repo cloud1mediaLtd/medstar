3e1f554f19acb2bb1f8b42fac4bc0277fada55c1
Anas Mahmud <admin@cloud1media.com>
2023-05-09 12:25:45 +0100

    added party town

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,29 +5,23 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import alpinejs from "@astrojs/alpinejs";
 
-// https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
 import sanity from "astro-sanity";
 
 // https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://medastra.co.uk",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    sitemap(),
-    alpinejs(),
-    react(),
-    sanity({
-      projectId: "df3si275",
-      dataset: "production",
-      apiVersion: "2023-02-08",
-      useCdn: false,
-    }),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), mdx(), sitemap(), alpinejs(), react(), sanity({
+    projectId: "df3si275",
+    dataset: "production",
+    apiVersion: "2023-02-08",
+    useCdn: false
+  }), partytown()]
 });
